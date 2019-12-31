71f57cc312c077d09742cc4c881b6b68a837c6ad
supernev <241907@qq.com>
2019-12-31 19:16:52 +0800

    Handle an event with multiple handlers

--- usecase/simple_event_manager.go
+++ usecase/simple_event_manager.go
@@ -2,25 +2,28 @@ package usecase
 
 // SimpleEventManager - A simple manager deal with events
 type SimpleEventManager struct {
-	handlers map[EventID]func(IEvent)
+	handlers map[EventID][]func(IEvent)
 }
 
 // GetSimpleEventManager - Static function to get
 func GetSimpleEventManager() *SimpleEventManager {
 	return &SimpleEventManager{
-		handlers: map[EventID]func(IEvent){},
+		handlers: map[EventID][]func(IEvent){},
 	}
 }
 
 // Register - Register function to handle a event
 func (m *SimpleEventManager) Register(evid EventID, fn func(ev IEvent)) {
-	m.handlers[evid] = fn
+	m.handlers[evid] = append(m.handlers[evid], fn)
 }
 
 // Dispatch - a event
 func (m *SimpleEventManager) Dispatch(evid EventID, ev IEvent) {
-	var fn = m.handlers[evid]
-	if fn != nil {
-		fn(ev)
+	var li, ok = m.handlers[evid]
+
+	if ok {
+		for _, fn := range li {
+			fn(ev)
+		}
 	}
 }
